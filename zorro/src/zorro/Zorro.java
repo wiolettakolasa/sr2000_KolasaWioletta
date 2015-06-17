5bd6eb80d92b367cf397c617dbc73d91fce4e06e
sobytek <aghlaboratorium1235@gmail.com>
2015-06-17 18:41:13 +0200

    Zorro tu był!

---
+++ zorro/src/zorro/Zorro.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zorro;
+
+/**
+ *
+ * @author student207
+ */
+public class Zorro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       for(int k=0; k<10; k++)
+           {System.out.print("*");
+               }
+        for(int i=0; i<10; i++)
+        {       System.out.print(" ");
+                for(int j=0; j<10; j++)
+                {System.out.print("*");
+                }
+                
+    for(int l=0; l<10; l++)
+           {System.out.print("*");
+               }
+        }
+        
+        
+    
+}
+}
